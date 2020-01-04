f0635a65dafc1d4a4a1f087f7be687a5a3367f72
fronoske <fronoske@gmail.com>
2020-01-04 15:05:52 +0900

    output more log

--- build-chm.cmd
+++ build-chm.cmd
@@ -2,7 +2,11 @@ git clone https://github.com/fronoske/bitclust.git
 cd bitclust
 git clone https://github.com/rurema/doctree.git rubydoc
 bundle install --path=vendor/bundle
+echo finish bundle install
 bundle exec bitclust -d ./db init encoding=utf-8 version=2.7.0
+echo finish bundle exec bitclust init
 bundle exec bitclust -d ./db update --stdlibtree=./rubydoc/refm/api/src
+echo finish bundle exec bitclust update
 if exist chm ( rmdir /S /Q chm )
 bundle exec bitclust -d ./db chm -o ./chm
+echo finish bundle exec bitclust chm
